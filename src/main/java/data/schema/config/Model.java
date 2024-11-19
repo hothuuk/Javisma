6487d9752327fe41381a2839415b3a1ae14ec54f
hothuuk <kijkj123@gmail.com>
2024-11-19 17:27:44 +0900

    Create configs

---
+++ src/main/java/data/schema/config/Model.java
@@ -0,0 +1,16 @@
+package data.schema.config;
+
+import java.util.List;
+
+public class Model {
+
+    private List<Field> fields;
+
+    public List<Field> getFields() {
+        return fields;
+    }
+
+    public void setFields(List<Field> fields) {
+        this.fields = fields;
+    }
+}
