2b77b93bc1a5010a1700512ef64668b6c2155fda
hothuuk <kijkj123@gmail.com>
2024-12-02 18:09:35 +0900

    Add field name validation

--- src/main/java/data/schema/parser/ModelParser.java
+++ src/main/java/data/schema/parser/ModelParser.java
@@ -3,17 +3,30 @@ package data.schema.parser;
 import data.schema.config.Field;
 import data.schema.config.Model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ModelParser {
+
+    // List of allowed types.
+    private static final Set<String> VALID_TYPES = new HashSet<>(Arrays.asList("Int", "String"));
 
     public Model parse(Block block) {
         String blockContent = block.content();
         List<Field> fields = new ArrayList<>();
+        int lineNumber = 0;
 
         for (String line : blockContent.split("\n")) {
+            lineNumber++;
             String[] tokens = line.split(" ");
+
+            if (tokens.length == 1) {
+                if (VALID_TYPES.contains(tokens[0])) {
+                    throw new IllegalStateException(
+                            String.format("Field name is missing at line %d in the '%s' model.", lineNumber, block.name())
+                    );
+                }
+            }
+
             String name = tokens[0];
             String type = tokens[1];
 
